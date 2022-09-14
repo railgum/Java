8062857f701cc406b70b9cccaed8889a2a3d3814
Rail <railbolo@gmail.com>
2022-09-14 21:16:45 +0400

    start

--- src/JavaException_lesson1/HomeWork.java
+++ src/JavaException_lesson1/HomeWork.java
@@ -1,7 +1,40 @@
 package JavaException_lesson1;
 
+import java.io.File;
+import java.io.FileReader;
+
+
 public class HomeWork {
     public static void main(String[] args) {
-
+        sum2d();
     }
+    /* 1 Задание*/
+    // 1)
+        public static void f1() {
+        File f = new File("X://java/file.txt");
+            FileReader fr = new FileReader(f);
+        }
+    //2)
+        public static void f2() {
+        System.out.println("Чтобы понять рекурсию, нужно понять рекурсию!");
+            f2();
+        }
+    //3
+        public static void f3() {
+            int[] arr = null;
+            int a = arr.length;
+            System.out.println(a);
+        }
+    public static int sum2d(String[][] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < 5; j++) {
+                int val = Integer.parseInt(arr[i][j]);
+                sum += val;
+            }
+        }
+        return sum;
+    }
+
+
 }
