a9d5b3616bcf9592f24bfdf13e7a18215d26d118
Rail <railbolo@gmail.com>
2022-09-17 18:05:28 +0400

    new

--- src/JavaException_lesson2/HomeWork2.java
+++ src/JavaException_lesson2/HomeWork2.java
@@ -1,7 +1,23 @@
 package JavaException_lesson2;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class HomeWork2 {
   public static void main(String[] args) {
+    givMeFloat();
+  }
+
+  public static float givMeFloat() {
+     Scanner scanner = new Scanner(System.in);
+     System.out.println("Введите дробное число");
+
+     if (scanner.hasNextFloat()) {
+      float number = scanner.nextFloat();
+      return number;
+     } else {
+      System.out.println("Извините, это не дробное число. Попробуйте ещё раз");
+     }
     
   }
 }
