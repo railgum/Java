84cc1ba112f9f855d753ff594fa1d706133c2f7d
Rail <railbolo@gmail.com>
2022-09-18 22:28:06 +0400

    new

--- src/JavaException_lesson2/HomeWork2.java
+++ src/JavaException_lesson2/HomeWork2.java
@@ -1,23 +1,37 @@
 package JavaException_lesson2;
 
 import java.util.InputMismatchException;
+//import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class HomeWork2 {
   public static void main(String[] args) {
-    givMeFloat();
+    giveMeFloat();
   }
 
-  public static float givMeFloat() {
-     Scanner scanner = new Scanner(System.in);
-     System.out.println("Введите дробное число");
-
-     if (scanner.hasNextFloat()) {
-      float number = scanner.nextFloat();
-      return number;
-     } else {
-      System.out.println("Извините, это не дробное число. Попробуйте ещё раз");
-     }
-    
+  public static float giveMeFloat() {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Введите дробное число");
+    float number = 0.0f;
+    boolean isFloat = false;
+    do {
+      try {
+        number = scanner.nextFloat();
+        if (!scanner.hasNextInt()) {
+          System.out.println("Ваше число " + number);
+          isFloat = true;
+        } else {
+          isFloat = false;
+          System.out.println("Введите число с дробной частью через ЗАПЯТУЮ");
+          // String line = scanner.nextLine();
+        }
+      } catch (InputMismatchException e) {
+        System.err.println("Это не число. Попробуйте ещё раз");
+        isFloat = false;
+        String line = scanner.nextLine();
+      }
+    } while (!isFloat);
+    return number;
   }
+
 }
