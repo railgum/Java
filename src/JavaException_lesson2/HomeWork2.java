9b1c2c86b7f822477b3844aa0958d709e18a6b50
Rail <railbolo@gmail.com>
2022-09-20 08:17:41 +0400

    new

--- src/JavaException_lesson2/HomeWork2.java
+++ src/JavaException_lesson2/HomeWork2.java
@@ -1,84 +1,131 @@
 package JavaException_lesson2;
 
-import java.util.InputMismatchException;
+//import java.io.FileNotFoundException;
 //import java.util.InputMismatchException;
+//import java.util.Scanner;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class HomeWork2 {
-  public static void main(String[] args) {
-    // giveMeFloat();
-    f1();
-  }
-
-  // 1 задание
-  public static float giveMeFloat() {
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Введите дробное число");
-    float number = 0.0f;
-    boolean isFloat = false;
-    do {
-      try {
-        number = scanner.nextFloat();
-        if (!scanner.hasNextInt()) {
-          System.out.println("Ваше число " + number);
-          isFloat = true;
-        } else {
-          isFloat = false;
-          System.out.println("Введите число с дробной частью через ЗАПЯТУЮ");
-          // String line = scanner.nextLine();
-        }
-      } catch (InputMismatchException e) {
-        System.err.println("Это не число. Попробуйте ещё раз");
-        isFloat = false;
-        String line = scanner.nextLine();
-      }
-    } while (!isFloat);
-    return number;
-  }
+  /**
+   * @param args
+   */
+  // public static void main(final String[] args) {
+  // giveMeFloat();
+  // f1();
+  // }
+  /*
+   * // 1 задание
+   * public static void giveMeFloat() {
+   * Scanner scanner = new Scanner(System.in);
+   * System.out.println("Введите дробное число");
+   * float number;
+   * boolean isFloat = false;
+   * while (!isFloat) {
+   * try {
+   * if (!scanner.hasNextInt()) {
+   * number = scanner.nextFloat();
+   * System.out.println("Ваше число " + number);
+   * isFloat = true;
+   * } else {
+   * isFloat = false;
+   * System.out.println("Введите число с дробной частью через ЗАПЯТУЮ");
+   * String line = scanner.nextLine();
+   * }
+   * } catch (InputMismatchException e) {
+   * System.err.println("Это не число. Попробуйте ещё раз");
+   * isFloat = false;
+   * String line = scanner.nextLine();
+   * }
+   * }
+   * }
+   */
 
   // 2 задание
-  public static void f1() {
-
-    try
-
-    {
-      int d = 0;
-      int[] intArray = new Int[8];
-      for (int i : intArray) {
-        double catchedRes1 = i / d;
-        System.out.println("catchedRes1 = " + catchedRes1);
-      }
-    } catch (ArithmeticException e) {
-      System.out.println("Catching exception: " + e);
-    }
-  }
+  /**
+   * 
+   * public static void f1() {
+   * 
+   * try
+   * 
+   * {
+   * int d = 0;
+   * int[] intArray = new int[8];
+   * for (int i : intArray) {
+   * double catchedRes1 = i / d;
+   * System.out.println("catchedRes1 = " + catchedRes1);
+   * }
+   * } catch (ArithmeticException e) {
+   * System.out.println("Catching exception: " + e);
+   * }
+   * }
+   */
 
   // 3 задание
 
-  public static void main(String[] args) throws Exception {
-    try {
-      int a = 90;
-      int b = 3;
-      System.out.println(a / b);
-      printSum(23, 234);
-      int[] abc = { 1, 2 };
-      abc[3] = 9;
-    } catch (Throwable ex) {
-      System.out.println("Что-то пошло не так...");
-    } catch (NullPointerException ex) {
-      System.out.println("Указатель не может указывать на null!");
-    } catch (IndexOutOfBoundsException ex) {
-      System.out.println("Массив выходит за пределы своего размера!");
+  /*
+   * public static void main(String[] args) throws Exception {
+   * try {
+   * int a = 90;
+   * int b = 3;
+   * System.out.println(a / b);
+   * printSum(23, 234);
+   * int[] abc = { 1, 2 };
+   * abc[3] = 9;
+   * } catch (NullPointerException ex) {
+   * System.out.println("Указатель не может указывать на null!");
+   * } catch (IndexOutOfBoundsException ex) {
+   * System.out.println("Массив выходит за пределы своего размера!");
+   * } catch (Throwable ex) {
+   * System.out.println("Что-то пошло не так...");
+   * }
+   * }
+   * 
+   * public static void printSum(Integer a, Integer b) throws IOException {
+   * FileReader test = null;
+   * try {
+   * test = new FileReader("test");
+   * test.read();
+   * System.out.println(a + b);
+   * } catch (IOException e) {
+   * System.err.println("Файл не найден");
+   * } finally {
+   * if (test != null) {
+   * test.close();
+   * }
+   * }
+   */
+
+  // 4 задание
+  /*
+   * 
+   * Разработайте программу, которая
+   * выбросит Exception, когда пользователь
+   * вводит пустую
+   * строку. Пользователю должно
+   * показаться сообщение,*
+   * что пустые
+   * строки вводить нельзя.
+   */
+
+   public class EmptyString extends Exception {
+    public EmptyString(String message) {
+      super(message);
+    }
+   }
+  public static void main(String[] args) throws EmptyString {
+    Scanner scan = new Scanner(System.in);
+    String line = scan.nextLine();
+    checkNonEmptyString(line);
+  }
+
+  public static void checkNonEmptyString(String str) throws EmptyString {
+    if (str == null || str.isEmpty() || str.trim().isEmpty()) {
+      throw new EmptyString("Нельзя вводить пустые строки!");
     }
   }
 
-  public static void printSum(Integer a, Integer b) throws FileNotFoundException {
-    System.out.println(a + b);
-  }
-  // 4 задание
-  /*
-   * Разработайте программу, которая выбросит Exception, когда пользователь
-   * вводит пустую строку. Пользователю должно показаться сообщение,
-   * что пустые строки вводить нельзя.
-   */
+  
+
 }
