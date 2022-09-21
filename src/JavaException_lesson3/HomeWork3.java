6a4db22f0cc9b3f549f61f05543c13811a7f9a1f
Rail <railbolo@gmail.com>
2022-09-21 19:36:18 +0400

    new

--- src/JavaException_lesson3/HomeWork3.java
+++ src/JavaException_lesson3/HomeWork3.java
@@ -1,5 +1,14 @@
 package JavaException_lesson3;
 
+import java.util.Scanner;
+
 public class HomeWork3 {
-  
+  public static void main(String[] args) {
+    
+  }
+
+  public static void getForm() {
+    Scanner scanner = new Scanner(System.in);
+    
+  }
 }
