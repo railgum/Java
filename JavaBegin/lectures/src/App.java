252952350bb6dc940a1c089343d6b5eccc54c946
Rail <railbolo@gmail.com>
2023-02-12 12:34:19 +0400

    add gitignore

--- JavaBegin/lectures/src/App.java
+++ JavaBegin/lectures/src/App.java
@@ -1,5 +1,18 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+  public static void main(String[] args) {
+    try (FileWriter fw = new FileWriter("testfile.txt", true)) {
+      fw.write("line 1");
+      fw.append("\n");
+      fw.append("2");
+      fw.append("\n");
+      fw.write("line 3");
+      fw.append("\n");
+      fw.flush();
+    } catch (IOException ex) {
+      System.out.println(ex.getMessage());
     }
+  }
 }
