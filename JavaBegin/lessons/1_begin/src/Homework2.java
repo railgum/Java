45aa27640fed92b5ac3e57acef0c4f08533aca4c
Rail <railbolo@gmail.com>
2023-02-15 06:17:40 +0400

    begin homework2

---
+++ JavaBegin/lessons/1_begin/src/Homework2.java
@@ -0,0 +1,16 @@
+
+public class Homework2 {
+    public static void main(String[] args) {
+        // Написать метод который приводит строку к нормальному виду
+        // строка на вход:
+        // Добрый день Как дела Все хорошо
+        // выход:
+        // Добрый день. Как дела. Все хорошо.
+
+        stringNormal("Добрый день Как дела Все хорошо");
+    }
+
+    static String stringNormal(String str) {
+
+    }
+}
