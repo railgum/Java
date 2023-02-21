5fb17023ffa18475a99ec3a35698df396d9a0cad
Rail <railbolo@gmail.com>
2023-02-21 18:25:34 +0400

    add branch hw4_1

--- JavaBegin/lessons/1_begin/src/Homework4.java
+++ JavaBegin/lessons/1_begin/src/Homework4.java
@@ -1,61 +1,78 @@
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
+import
 
 public class Homework4 {
-    public static void main(String[] args) {
-        /*
-        * 1 Реализовать телефонный справочник
+    /*
+          Реализовать телефонный справочник
           HashMap<Login, Список телефонов> с возможностью:
             - добавлять телефон
             - удалять телефон
             - находить по логину телефон/список телефонов
-        */
-
-        HashMap<String, HashMap> phoneBook = new HashMap<>();
-        menu(phoneBook);
-
+     */
+    private final Scanner scanner;
+    public Homework4(Scanner scanner) {
+        this.scanner = scanner;
     }
-    static void menu(HashMap phB){
-        System.out.print("\033[H\033[2J");
+    private void printMenu(){
+        //System.out.print("\033[H\033[2J");
         System.out.println("Телефонный справочник");
         System.out.println("Выберите нужный пункт");
         System.out.println("1 - Добавить телефон");
         System.out.println("2 - Удалить телефон");
         System.out.println("3 - Найти телефон по логину");
-        Scanner scanner = new Scanner(System.in);
-        String num = scanner.nextLine();
-        scanner.close();
-        switch (num) {
-            case "1": addNumber(phB);
-            break;
-            case "2": deleteNumber(phB);
-            break;
-            case "3": searchNumber(phB);
-            break;
-            default:
-                System.out.println("Выберите нужный пункт");
+        System.out.println("4 - Выход");
+    }
+
+    public void run(pB){
+        if (this.scanner != null){
+            int number;
+            do{
+                printMenu();
+                number=this.scanner.nextInt();
+                switch (number){
+                    case 1: addNumber(pB);
+                        break;
+                    case 2: deleteNumber(pB);
+                        break;
+                    case 3: searchNumber(pB);
+                        break;
+                    case 4:
+                        System.out.println("Всего хорошего!...");
+                        break;
+                    default:
+                        System.out.println("Выберите нужный пункт");
+                }
+            } while (number!=4);
         }
     }
-    static void addNumber(HashMap phB){
+    public static void main(String[] args) {
+        HashMap<String, HashMap> phoneBook = new HashMap<>();
+        run(phoneBook);
+    }
+    static void addNumber(HashMap pB){
         Scanner scanner = new Scanner(System.in);
 
-        String login = scanner.nextLine();
         System.out.println("Введите логин");
+        String login = scanner.nextLine();
+
         scanner.nextLine();
         String number = scanner.nextLine();
         System.out.println("Введите номер телефона");
 
         scanner.close();
-        int k=0;
+        //int k=0;
         if (!phB.containsKey(login)) {
             phB.put(login,number);
         } else phB.putIfAbsent(login,number);
     }
     static void deleteNumber(HashMap phB){
         System.out.println("delete");
+        menu(phB);
     }
     static void searchNumber(HashMap phB) {
         System.out.println("search");
+        menu(phB);
+
     }
 }
