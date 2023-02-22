7edebdac0674254ef78f16d651f1fa0f528a9630
Rail <railbolo@gmail.com>
2023-02-22 05:54:41 +0400

    end hw4

--- JavaBegin/lessons/1_begin/src/Homework4.java
+++ JavaBegin/lessons/1_begin/src/Homework4.java
@@ -3,9 +3,7 @@ import com.sun.jdi.Value;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
+import java.util.*;
 
 public class Homework4 {
     /*
@@ -15,71 +13,89 @@ public class Homework4 {
             - удалять телефон
             - находить по логину телефон/список телефонов
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         HashMap<String, HashSet> phoneBook = new HashMap<>();
-        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            int x = 0;
-            String s = "";
-            while (!"4".equals(s)) {
-                System.out.println("Телефонный справочник");
-                System.out.println("Выберите нужный пункт");
-                System.out.println("1 - Добавить телефон");
-                System.out.println("2 - Удалить телефон");
-                System.out.println("3 - Найти телефон по логину");
-                System.out.println("4 - Выход");
-                s = br.readLine();
+        // Меню справочника
+        Scanner br = new Scanner(System.in);
+        int x = 0;
+        String s = "";
+        // Циклический опрос консоли
+        do {
+            System.out.println("Телефонный справочник");
+            System.out.println("Выберите нужный пункт");
+            System.out.println("1 - Добавить телефон");
+            System.out.println("2 - Удалить телефон");
+            System.out.println("3 - Найти телефон по логину");
+            System.out.println("4 - Выход");
+            s = br.next();
 
-                try {
-                    x = Integer.parseInt(s);
-                } catch (NumberFormatException e) {
-                    System.out.println("Неверный ввод");
-                }
-
-                switch (x) {
-                    case 1:
-                        addNumber(phoneBook);
-                        break;
-                    case 2:
-                        // вызов метода 2
-                        break;
-                    case 3:
-                        // вызов метода 3
-                }
+            try {
+                x = Integer.parseInt(s);
+            } catch (NumberFormatException e) {
+                System.out.println("Неверный ввод");
             }
-            System.out.println("Доброго дня!");
-        } catch (IOException exception){
-            System.out.println(exception.getMessage());
-        }
-    }
-    static void deleteNumber(HashMap phB){
-        System.out.println("delete");
-    }
-    static void searchNumber(HashMap phB) {
-        System.out.println("search");
-    }
 
-    private static void addNumber(HashMap<String, HashSet> phoneBook) {
-        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            System.out.println("Введите логин");
-            String login = br.readLine();
-            System.out.println("Введите номер телефона");
-            String number = br.readLine();
-            if (!phoneBook.containsKey(login)) {
-                HashSet<String> phoneNumber = new HashSet<>();
-                phoneNumber.add(number);
-                phoneBook.put(login,new HashSet<>(phoneNumber));
-            } else {
-                HashSet<String> phoneNumber = phoneBook.get(login);
-                phoneNumber.add(number);
-                phoneBook.putIfAbsent(login,phoneNumber);
-            };
-            for (Map.Entry<String,HashSet> item : phoneBook.entrySet()) {
-                System.out.println("Логин - " + item.getKey());
-                System.out.println("Телефон - " + item.getValue());
+            switch (x) {
+                case 1:
+                    System.out.println("Добавление номера");
+                    String login = getContact("Введите логин");
+                    String number = getContact("Введите номер");
+                    if (!phoneBook.containsKey(login)) {
+                        HashSet<String> phoneNumber = new HashSet<>();
+                        phoneNumber.add(number);
+                        phoneBook.put(login,new HashSet<>(phoneNumber));
+                    } else {
+                        HashSet<String> phoneNumber = phoneBook.get(login);
+                        phoneNumber.add(number);
+                        phoneBook.putIfAbsent(login,phoneNumber);
+                    };
+                    for (Map.Entry<String,HashSet> item : phoneBook.entrySet()) {
+                        System.out.println("Логин - " + item.getKey());
+                        System.out.println("Телефон - " + item.getValue());
+                    }
+                    break;
 
+                case 2:
+                    System.out.println("Удаление номера");
+                    login = getContact("Введите логин");
+                    number = getContact("Введите номер");
+                    if (!phoneBook.containsKey(login)) {
+                        System.out.println("Такого контакта нет");
+                    } else {
+                        HashSet<String> phoneNumber = phoneBook.get(login);
+                        phoneNumber.remove(number);
+                    };
+                    for (Map.Entry<String,HashSet> item : phoneBook.entrySet()) {
+                        System.out.println("Логин - " + item.getKey());
+                        System.out.println("Телефон - " + item.getValue());
+
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Поиск номера");
+                    login = getContact("Введите логин");
+                    if (!phoneBook.containsKey(login)) {
+                        System.out.println("Такого контакта нет");
+                    } else {
+                        HashSet<String> phoneNumber = phoneBook.get(login);
+                        Iterator<String> iterator = phoneNumber.iterator();
+                        while (iterator.hasNext()){
+                            System.out.println(iterator.next());
+                        }
+                    }
             }
-        } catch (IOException exception){
-            System.out.println(exception.getMessage());
-        }
+        } while (!"4".equals(s));
+        System.out.println("Доброго дня!");
+        br.close();
     }
+    public static String getContact(String message){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(message);
+        String value = scanner.next();
+        return value;
+
+    }
+
 }
+
