19566e540c053c29a1558082d90ee3f8d2fb4495
Rail <railbolo@gmail.com>
2023-03-01 03:26:40 +0400

    end hw5

--- JavaBegin/lessons/1_begin/src/Homework5.java
+++ JavaBegin/lessons/1_begin/src/Homework5.java
@@ -15,28 +15,42 @@ public class Homework5 {
         //Output: 5
         //Explanation: Повторяющийся подмассив с максимальной длиной равен [0,0,0,0,0].
         
-        int[] nums1 = {1,2,3,2,1};
+        int[] nums1 = {1,2,3,2,1,4,7};
         int[] nums2 = {3,2,1,4,7};
-        arrayRepeatValue(nums1,nums2);
+        System.out.println(arrayRepeatValue(nums1,nums2));
     }
 
     public static ArrayList<Integer> arrayRepeatValue(int[] arr1, int[] arr2){
-        List<Integer> res = new ArrayList<>();
-        if (arr1.length>arr2.length){
-            res = searchRepeatValue(arr1,arr2);
-        }
-        else res = searchRepeatValue(arr2,arr1);
+        List<Integer> res = (arr1.length>arr2.length) ? searchRepeatValue(arr1,arr2) : searchRepeatValue(arr2,arr1);
         return (ArrayList<Integer>) res;
     }
 
     private static List<Integer> searchRepeatValue(int[] arr1, int[] arr2) {
         List<Integer> res = new ArrayList<>();
-        for (int i = 0; i < arr2.length; i++) {
-            for (int j = 0; j < arr1.length; j++) {
-                if (arr2[i]==arr1[j]){
-
-                }
+        for (int i = 0; i < arr1.length-1; i++) {
+            for (int j = 0; j <= arr2.length-1; j++) {
+                if(arr1[i]==arr2[j]){
+                    res.add(arr1[i]);
+                    i++;
+                } else res.clear();
             }
         }
+// через while
+        /*
+        int i=0;
+        while (i<arr1.length-1) {
+            int j=0;
+            while (j<=arr2.length-1){
+                if (arr1[i] == arr2[j]){
+                    res.add(arr1[i]);
+                    i++;
+                }
+                j++;
+            }
+            i++;
+        }
+        */
+        return res;
     }
+
 }
