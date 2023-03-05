e8a1468b1a7d47712f885e5f4020105d968f0512
Rail <railbolo@gmail.com>
2023-03-05 20:45:47 +0400

    start task3

---
+++ JavaOOP/OOP/Lesson_1/Homework/Locker/Locker.java
@@ -0,0 +1,13 @@
+package Lesson_1.Homework.Locker;
+
+public class Locker {
+
+    private boolean status;
+    public Locker(){
+        this.status = getStatus();
+    }
+
+    public boolean getStatus() {return status;}
+    public void setStatus(boolean status) { this.status = status;}
+}
+
